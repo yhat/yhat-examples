ee5070f5f5d81a0c948dc71e6033728d690ec319
Greg Lamp <lamp.greg@gmail.com>
2013-08-27 18:24:21 -0400

    added movie recommender for R

---
+++ movie-recommender/R/main.R
@@ -0,0 +1,83 @@
+setwd("~/repos/yhat/yhat-examples/movie-recommender/R/")
+
+# data from: http://www.grouplens.org/node/73
+data <- read.table("./data/movielens.txt", header=TRUE, stringsAsFactors=FALSE)
+data <- data[order(data$user_id, data$movie_id),]
+
+movies <- read.csv("./data/movies.txt", header=TRUE, stringsAsFactors=FALSE, sep="|")
+head(movies)
+head(data)
+
+find_common_reviewers <- function(movieA, movieB) {
+  movieAViewers <- subset(data, movie_id==movieA)$user_id
+  movieBViewers <- subset(data, movie_id==movieB)$user_id
+  commonViewers <- intersect(movieAViewers, movieBViewers)
+  commonViewers
+}
+
+get_review_correlation <- function(movieA, movieB) {
+  common <- find_common_reviewers(movieA, movieB)
+  cor(subset(data, user_id %in% common & movie_id==movieA)$rating,
+      subset(data, user_id %in% common & movie_id==movieB)$rating)
+}
+# show an example
+get_review_correlation(1, 7)
+
+# install.packages("plyr")
+library(plyr)
+
+# find movies with >300 reviews
+movies.count <- ddply(data, .(movie_id), nrow)
+movies.count <- movies.count[movies.count$V1 > 300,]
+
+# limit our data to movies w/ large sample size
+data <- subset(data, movie_id %in% movies.count$movie_id)
+movies <- subset(movies, id %in% unique(data$movie_id))
+head(movies[,1:2])
+
+
+rec_movies <- function(movieTitle) {
+  id <- subset(movies, title==movieTitle)$id
+  # compute the correlation between the movie a user specified
+  # and each title
+  ddply(movies, .(id, title), function(movie) {
+    c("similarity"=get_review_correlation(id, movie$id))
+  })
+}
+
+rec_movies("Indiana Jones and the Last Crusade (1989)")
+
+
+library(yhatr)
+
+model.require <- function() {
+  library(plyr)
+}
+
+model.transform <- function(df) { df }
+
+model.predict <- function(df) {
+  recs <- rec_movies(df$title)
+  # reorder from most to least similar
+  o <- order(recs$similarity, decreasing=TRUE)
+  recs <- recs[o,]
+  # remove the same movie
+  recs[-1,]
+}
+
+yhat.config <- c(
+  username="greg",
+  apikey="fCVZiLJhS95cnxOrsp5e2VSkk0GfypZqeRCntTD1nHA"
+)
+
+yhat.deploy("movieRecommender")
+# define what our inputs to the recommender will be:
+#   unique, sorted list of movies w/ reviews>300
+sample.data <- data.frame(title=sort(unique(movies$title)))
+# try twice
+yhat.document("movieRecommender", 2, sample.data)
+# open the URL to view the model:
+#   http://yhathq.com/models/greg/521d2566b517e80e324075a5
+
+
+
